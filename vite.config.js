c2d56a290bac7cb81a72e0cf6f9b3212cc64faba
omvyas-dev <vyasom111@gmail.com>
2025-08-07 05:20:32 -0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/choco/', // ← add this line
+  base: '/choco/', // ✅ must match repo name
   plugins: [react()],
 })
