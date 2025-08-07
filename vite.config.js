2729a8eb9f0a267253bc7d62eed14d068d8e33db
omvyas-dev <vyasom111@gmail.com>
2025-08-07 05:48:02 -0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/choco/', // ✅ must match repo name
+  base: '/docto/', // 👈 important for GitHub Pages
   plugins: [react()],
-})
+});
